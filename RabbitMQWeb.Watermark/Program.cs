99b0221e9f64192a111fe1066aca35c03b1f6f3f
BatuhanBedir <114083704+BatuhanBedir@users.noreply.github.com>
2023-08-21 13:54:36 +0300

    addWatermarkToImage

--- RabbitMQWeb.Watermark/Program.cs
+++ RabbitMQWeb.Watermark/Program.cs
@@ -1,36 +1,58 @@
-namespace RabbitMQWeb.Watermark
+using Microsoft.EntityFrameworkCore;
+using RabbitMQ.Client;
+using RabbitMQWeb.Watermark.BackgroundServices;
+using RabbitMQWeb.Watermark.Models;
+using RabbitMQWeb.Watermark.Services;
+using System.Configuration;
+
+namespace RabbitMQWeb.Watermark;
+
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddSingleton(sp => new ConnectionFactory()
         {
-            var builder = WebApplication.CreateBuilder(args);
+            Uri = new Uri(builder.Configuration.GetConnectionString("RabbitMQ")),
+            DispatchConsumersAsync = true, //asenkron kullanýldýðý için
+        });
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+        builder.Services.AddHostedService<ImageWatermarkProcessBackgroundService>();
 
-            var app = builder.Build();
+        builder.Services.AddSingleton<RabbitMQClientService>();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+        builder.Services.AddSingleton<RabbitMQPublisher>();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+        builder.Services.AddDbContext<AppDbContext>(options =>
+        {
+            options.UseInMemoryDatabase(databaseName: "productDb");
+        });
+        // Add services to the container.
+        builder.Services.AddControllersWithViews();
 
-            app.UseRouting();
+        var app = builder.Build();
 
-            app.UseAuthorization();
-
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
-
-            app.Run();
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
         }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+
+        app.Run();
     }
 }
